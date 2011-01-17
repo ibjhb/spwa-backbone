0f888c9d3af54c3752898b30e603e4c981355146
James Brown <james@gulftobaytech.com>
2011-01-17 18:02:04 -0500

    Changing ' to " for consistency.

--- spwa-backbone-0.9.js
+++ spwa-backbone-0.9.js
@@ -21,7 +21,7 @@ $(function(){
 		model: page
 	});
 	
-	// Create a 'pages' object which contains a collection of page models:
+	// Create a "pages" object which contains a collection of page models:
 	window.pages = new sitepages;	
 	
 	// Define our page view:
@@ -33,11 +33,11 @@ $(function(){
 		// This runs when a new view is made:
 		,initialize: function(){
 			
-			// Keep render and close bound to 'this':
-			_.bindAll(this, 'render', 'close');
+			// Keep render and close bound to "this":
+			_.bindAll(this, "render", "close");
 			
-			// When 'change' is called, let's render the view:
-  			this.model.bind('change', this.render);
+			// When "change" is called, let's render the view:
+  			this.model.bind("change", this.render);
 		
 			// Setting the view into the model:
   			this.model.view = this;
@@ -47,7 +47,7 @@ $(function(){
 		
 		// This function takes the html from the model, inserts into the view and renders it to the screen:
 		,render: function() {
-			$(this.el).html($(this.model.get('htmlTemplate')).html()).attr('id', this.model.get('id'));
+			$(this.el).html($(this.model.get("htmlTemplate")).html()).attr("id", this.model.get("id"));
 			return this;
 		}
 	});
@@ -56,32 +56,32 @@ $(function(){
 	window.site = Backbone.Controller.extend({
 		
 		 // Set the app to the page body:
-		 el:		$('body')
+		 el:		$("body")
 		 
 		 // This defines our routes and calls show when a route is encountered:
 		,routes: {
-					"!:pageName": 'show'
+					"!:pageName": "show"
 		},
 		
 		// Fire up when the site object is called.  We pass in a jQuery array of script objects:
 		initialize: function(contentPages){
 			
-			// Loop over each object, define some attributes and push them into the 'pages' collection:
+			// Loop over each object, define some attributes and push them into the "pages" collection:
 			contentPages.each(function(i, contentPage){
 				var contentPage = $(contentPage);
 				var page = {
-					 id: 			contentPage.attr('id').split('-')[0]						 
-					,htmlTemplate:	'#' + contentPage.attr('id')
+					 id: 			contentPage.attr("id").split("-")[0]						 
+					,htmlTemplate:	"#" + contentPage.attr("id")
 				}
 				pages.add(page);
 			})
 			
-			// If there isn't a #!something set the site to 'home':
-			if (window.Backbone.history.getFragment() == ''){
-				this.show('home')
+			// If there isn't a #!something set the site to "home":
+			if (window.Backbone.history.getFragment() == ""){
+				this.show("home")
 			}
 		},
-		// This funciton does the actual page 'switching'.  It pulls the model out of the
+		// This funciton does the actual page "switching".  It pulls the model out of the
 		// collection of pages.  Then if the view doesn't exist, it creates it, if it does
 		// it loads it, then shows it in the body:
 		show: function(pageName) {
@@ -92,7 +92,7 @@ $(function(){
 	});
 	
 	// Fire up our applicaiton and pass in an array of script/html objects
-	window.App = new site($('script[type=text/template]'));
+	window.App = new site($("script[type=text/template]"));
 	
 	// Start hashtag tracking via Backbone:
 	Backbone.history.start();
